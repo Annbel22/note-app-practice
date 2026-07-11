9f6dc0233df0ac1602f2cb3d33f5ec06d26ea013
Annbel22 <annabelinskih7@gmail.com>
2026-07-11 03:04:55 +0300

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+call venv\Scripts\activate
+set QT_QPA_PLATFORM_PLUGIN_PATH=
+set QT_QPA_PLATFORM_PLUGIN_PATH=D:\Univer\Vumip\note_app\venv\Lib\site-packages\PyQt5\Qt5\plugins
+python main.py
+pause
